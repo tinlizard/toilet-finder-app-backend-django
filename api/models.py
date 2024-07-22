4e7e22e60d6714fb2020f867386b2f61867aa2d2
tinlizard <will.ben.weeks@gmail.com>
2024-07-22 11:20:58 +0200

    updated database

--- api/models.py
+++ api/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Toilet(models.Model):
+    name = models.CharField(max_length=100,default="Unknown Location")
     address = models.CharField(max_length=300)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     reviews = models.IntegerField(default=0)
-    toilet_id = models.IntegerField()
+    latitude = models.DecimalField(default=0.0, decimal_places=6,max_digits=10)
+    longitude = models.DecimalField(default=0.0, decimal_places=6,max_digits=10)
+    sexes = models.CharField(max_length=30,default="Male/Female")
 
     def __str__(self):
         return self.address
